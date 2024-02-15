e456465f58d3adcae46b2fbd484a1fa2ce4461e3
sovfor <146472805+sovfor@users.noreply.github.com>
2024-02-15 21:02:47 +0300

    Create exam.py
    Аттестационная работа

---
+++ exam.py
@@ -0,0 +1,17 @@
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+df = pd.DataFrame(columns =["human","robot"])
+print(df)
+
+data.loc[data['whoAmI'] == "human","human"] = "1"
+data.loc[data['whoAmI'] != "human","human"] = "0"
+
+data.loc[data['whoAmI'] == "robot","robot"] = "1"
+data.loc[data['whoAmI'] != "robot","robot"] = "0"
+
+data.pop('whoAmI')
+
+print(data)
